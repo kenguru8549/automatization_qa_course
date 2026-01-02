9e0e11bb33bcdb3520e1846479ba60ddc29791eb
Guskov-AV <kenguru.8549@gmail.com>
2026-01-02 16:55:29 +0300

    pervyy test2

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -10,3 +10,11 @@ class TextBoxPage(BasePage):
         self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('МО')  # заполнение поля...
         self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('Реутов')  # заполнение поля...
         self.element_is_visible(self.locators.SUBMIT).click()  #  проверяем видимость кнопки и нажимаем на нее
+
+
+    def check_filled_form(self):
+        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text  #  получаем текст методлом текст
+        email = self.element_is_present(self.locators.CREATED_EMAIL).text
+        current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text
+        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text
+        return full_name, email, current_address, permanent_address
