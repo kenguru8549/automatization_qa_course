bd4a96215075164d8e8d4376c87484e2ed11c432
Guskov-AV <kenguru.8549@gmail.com>
2026-02-15 12:42:10 +0300

    added class:   TestAlertsPage added tests: - test_see_alert - test_alert_appear_5_sec - test_check_confirm_alert - test_prompt_alert

--- pages/alerts_frame_windows_page.py
+++ pages/alerts_frame_windows_page.py
@@ -1,22 +1,57 @@
-from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators
+import random
+import time
+
+from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators, AlertsPageLocators
 from pages.base_page import BasePage
 
 
 class BrowserWindowsPage(BasePage):
     locators = BrowserWindowsPageLocators()
 
-    def check_opened_new_tab(self):
+    def check_opened_new_tab(self):  #  метод открытия новой вкладки
         self.element_is_visible(self.locators.NEW_TAB_BUTTON).click()  #  кликаем по кнопке открытия новой вкладки
         self.driver.switch_to.window(self.driver.window_handles[1])  #  переключаемся на открывшуюся вкладку с индексом 1
         text_title = self.element_is_present(self.locators.TITLE_NEW).text
         return text_title
 
-    def check_opened_new_window(self):
+    def check_opened_new_window(self):  #  метод открытия новой страницы
         self.element_is_visible(self.locators.NEW_WINDOW_BUTTON).click()  #  кликаем по кнопке открытия новой вкладки
         self.driver.switch_to.window(self.driver.window_handles[1])  #  переключаемся на открывшуюся вкладку с индексом 1
         text_title = self.element_is_present(self.locators.TITLE_NEW).text
         return text_title
 
+class AlertsPage(BasePage):
+    locators = AlertsPageLocators()
 
+    def check_see_alert(self):  #  метод появления попапа
+        self.element_is_visible(self.locators.SEE_ALERT_BUTTON).click()  #  нажимаем на кнопку для появления попапа
+        alert_window = self.driver.switch_to.alert  #  переключаемся на появившийся попап
+        return alert_window.text  #  возвращаем текст из попапа
+
+    def check_alert_appear_5_sec(self):  #  метод появления попапа
+        self.element_is_visible(self.locators.APPEAR_ALERT_AFTER_5_SEC_BUTTON).click()  #  нажимаем на кнопку для появления попапа
+        time.sleep(6)
+        try:  #  добавили обработку ошибки, чтобы кейс проходил
+            alert_window = self.driver.switch_to.alert
+            return alert_window.text
+        except UnexpectedAlertPresentException:
+            alert_window = self.driver.switch_to.alert
+            return alert_window.text
+
+    def check_confirm_alert(self):  #  метод появления попапа, нажатия на нем кнопки и проверки текста на осн. стр. после нажатия
+        self.element_is_visible(self.locators.CONFIRM_BOX_ALERT_BUTTON).click()  # нажимаем на кнопку для появления попапа
+        alert_window = self.driver.switch_to.alert  # переключаемся на появившийся попап
+        alert_window.accept()  #  нажимаем на ок в появившемся попапе, если нажать отмена, то будет метод дисмисс
+        text_result = self.element_is_present(self.locators.CONFIRM_RESULT).text  #  текст, который появится на осн. странице, после нажатия кнопки на попапе
+        return text_result
+
+    def check_prompt_alert(self):  #  метод появления попапа, написания в нем текста и проверки написанного
+        text = f'autotest{random.randint(0, 999)}'  #  текст, который будем записывать в попапе
+        self.element_is_visible(self.locators.PROMPT_BOX_ALERT_BUTTON).click()  # нажимаем на кнопку для появления попапа
+        alert_window = self.driver.switch_to.alert  # переключаемся на появившийся попап
+        alert_window.send_keys(text)  #  заполняем строку текстом
+        alert_window.accept()  #  нажимаем на ок в появившемся попапе, если нажать отмена, то будет метод дисмисс
+        text_result = self.element_is_present(self.locators.PROMPT_RESULT).text  #  текст, который появится на осн. странице, после нажатия кнопки на попапе
+        return text, text_result  #  возвращаем написанный текст и текст после нажатия кнопки
 
 
--- tests/alerts_frame_windows_tests.py
+++ tests/alerts_frame_windows_tests.py
@@ -1,20 +1,47 @@
 import time
 
-from pages.alerts_frame_windows_page import BrowserWindowsPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
 
 
 class TestAlertsFrameWindow:
 
     class TestBrowserWindows:
 
-        def test_new_tab(self, driver):
+        def test_new_tab(self, driver):  #  проверка открытия новой вкладки и текста на ней
             new_tab_page = BrowserWindowsPage(driver, 'https://demoqa.com/browser-windows')
             new_tab_page.open()
             test_result = new_tab_page.check_opened_new_tab()
             assert test_result == "This is a sample page", "Новая вкладка не открылась или не верный текст"
 
-        def test_new_window(self, driver):
+        def test_new_window(self, driver):  #  проверка открытия новой страницы
             browser_window_page = BrowserWindowsPage(driver, 'https://demoqa.com/browser-windows')
             browser_window_page.open()
             test_result = browser_window_page.check_opened_new_window()
             assert test_result == "This is a sample page", "Новая страница не открылась или не верный текст"
+
+
+    class TestAlertsPage:
+
+        def test_see_alert(self, driver):  #  проверка появления попапа
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_see_alert()  #  метод появления попапа с вытягиванием текста на нем
+            assert alert_text == 'You clicked a button'
+
+        def test_alert_appear_5_sec(self, driver):  #  проверка появления попапа после 5 секунд
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_alert_appear_5_sec()  #  метод появления попапа после 5 секунд с вытягиванием текста на нем
+            assert alert_text == 'This alert appeared after 5 seconds'
+
+        def test_check_confirm_alert(self, driver):  #  проверка появления попапа и нажатия на кнопку в нем
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_confirm_alert()  #  метод появления попапа нажатия кнопки и проверки текста
+            assert alert_text == 'You selected Ok'
+
+        def test_prompt_alert(self, driver):  #  проверка появления попапа, ввода в него текста и нажатия на кнопку в нем
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            text, alert_text = alert_page.check_prompt_alert()  #  метод появления попапа, ввода в него текста нажатия кнопки и проверки текста
+            assert alert_text == f'You entered {text}'  #  assert text in allert_text - второй вариант проверки
